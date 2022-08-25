0e04b3446171f2a7a5cf61fc37fbb88f75ae772b
01ricky18r <01ricky18r@gmail.com>
2022-08-25 09:34:28 +0530

    using props with regular javascript

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,7 +1,14 @@
 function Expenseitem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
   return (
-    <div className="flex justify-between bg-gray-400 m-10 p-4">
-      <div>{props.date.toISOString()}</div>
+    <div className="flex justify-between items-center bg-gray-400 m-10 p-4">
+      <div>
+        <div>{day}</div>
+        <div>{month}</div>
+        <div>{year}</div>
+      </div>
       <h2>{props.title}</h2>
       <div className="price">{props.amount}</div>
     </div>
