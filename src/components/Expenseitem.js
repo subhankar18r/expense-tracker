3023fa1330fc4b4f878b015af2c74e45db207e18
01ricky18r <01ricky18r@gmail.com>
2022-08-25 10:12:43 +0530

    spliting the expensedate componnet

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,14 +1,9 @@
+import ExpenseDate from "./ExpenseDate";
+
 function Expenseitem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
   return (
     <div className="flex justify-between items-center bg-gray-400 m-10 p-4">
-      <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <h2>{props.title}</h2>
       <div className="price">{props.amount}</div>
     </div>
