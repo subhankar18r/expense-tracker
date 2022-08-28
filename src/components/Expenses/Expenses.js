c9db2376c43dcff7d00aa792e9a01fa74e2af400
01ricky18r <01ricky18r@gmail.com>
2022-08-28 14:28:18 +0530

    fix bug

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,14 +4,16 @@ import Expenseitem from "./Expenseitem";
 import ExpensesFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState(new Date().getFullYear());
+  const [filteredYear, setFilteredYear] = useState(
+    new Date().getFullYear().toString()
+  );
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
   const filteredYearExpenses = props.item.filter((data) => {
-    return data.date.getFullYear() === filteredYear;
+    return data.date.getFullYear().toString() === filteredYear;
   });
 
   let expenseContent = <p>no expenses are added</p>;
