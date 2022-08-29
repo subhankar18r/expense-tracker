627fc6b1da91764551e2b87b09ad5b39328cd3cc
01ricky18r <01ricky18r@gmail.com>
2022-08-29 15:59:36 +0530

    initial commit

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,20 +1,23 @@
 import ChartBar from "./ChartBar";
 
 const Chart = (props) => {
+  const dataPointvalues = props.dataPoints.map((datapoint) => {
+    return datapoint.value;
+  });
+  const totalMaximum = Math.max(...dataPointvalues);
+
   return (
-    <div className="bg-purple-400 h-40 flex">
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
-      <ChartBar />
+    <div className="bg-purple-400 h-40 flex p-6 mt-4">
+      {props.dataPoints.map((dataPoint) => {
+        return (
+          <ChartBar
+            key={dataPoint.label}
+            value={dataPoint.value}
+            max={totalMaximum}
+            label={dataPoint.label}
+          />
+        );
+      })}
     </div>
   );
 };
