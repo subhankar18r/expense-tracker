906f2f856de7c296eb7d5d48149f95c61464cac0
01ricky18r <01ricky18r@gmail.com>
2022-08-29 16:00:34 +0530

    initial commit

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,10 +1,18 @@
 const ChartBar = (props) => {
+  let barFilHeight = "0%";
+  if (props.max > 0) {
+    barFilHeight = Math.round((props.value / props.max) * 100) + "%";
+  }
+
   return (
-    <div className="w-full p-3 text-center ">
-      <div className="inner bg-white  w-3 h-full m-auto rounded-md grid items-end">
-        <div className="fill w-full h-16 bg-black overflow-hidden rounded-md"></div>
+    <div className="w-full text-center h-full">
+      <div className="inner bg-white  w-3 h-full m-auto rounded-md flex items-end">
+        <div
+          className="fill w-full bg-black overflow-hidden rounded-md"
+          style={{ height: barFilHeight }}
+        ></div>
       </div>
-      <div></div>
+      <div>{props.label}</div>
     </div>
   );
 };
