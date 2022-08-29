66fb75fbf739dec47e1d2bd1a2c20659cd04d3a2
01ricky18r <01ricky18r@gmail.com>
2022-08-29 12:57:07 +0530

    initial commit

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,22 @@
+import ChartBar from "./ChartBar";
+
+const Chart = (props) => {
+  return (
+    <div className="bg-purple-400 h-40 flex">
+      <ChartBar />
+      <ChartBar />
+      <ChartBar />
+      <ChartBar />
+      <ChartBar />
+      <ChartBar />
+      <ChartBar />
+      <ChartBar />
+      <ChartBar />
+      <ChartBar />
+      <ChartBar />
+      <ChartBar />
+    </div>
+  );
+};
+
+export default Chart;
---
+++ src/components/Chart/ChartBar.js
@@ -0,0 +1,11 @@
+const ChartBar = (props) => {
+  return (
+    <div className="w-full p-3 text-center ">
+      <div className="inner bg-white  w-3 h-full m-auto rounded-md grid items-end">
+        <div className="fill w-full h-16 bg-black overflow-hidden rounded-md"></div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+export default ChartBar;
