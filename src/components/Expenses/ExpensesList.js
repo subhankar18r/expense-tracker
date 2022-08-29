78f23a8b2e78a9582b2931fd4fa186592ddd9a8b
01ricky18r <01ricky18r@gmail.com>
2022-08-29 09:57:16 +0530

    initial commit

---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,22 @@
+import Expenseitem from "./Expenseitem";
+
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className="text-center">no expenses found</h2>;
+  }
+  return (
+    <ul>
+      {props.items.map((expense) => {
+        return (
+          <Expenseitem
+            key={expense.id}
+            title={expense.title}
+            date={expense.date}
+            amount={"$" + expense.amount}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+export default ExpensesList;
