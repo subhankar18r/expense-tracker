aeb5e6927d10abe90cffec21951af9e2cb649915
01ricky18r <01ricky18r@gmail.com>
2022-08-29 09:56:31 +0530

    list the items

--- src/components/Expenses/Expenseitem.js
+++ src/components/Expenses/Expenseitem.js
@@ -2,11 +2,13 @@ import ExpenseDate from "./ExpenseDate";
 
 function Expenseitem(props) {
   return (
-    <div className="flex justify-between items-center bg-gray-800 text-white m-10 p-4">
-      <ExpenseDate date={props.date} />
-      <h2>{props.title}</h2>
-      <div className="price">{props.amount}</div>
-    </div>
+    <li>
+      <div className="flex justify-between items-center bg-gray-800 text-white m-10 p-4">
+        <ExpenseDate date={props.date} />
+        <h2>{props.title}</h2>
+        <div className="price">{props.amount}</div>
+      </div>
+    </li>
   );
 }
 
