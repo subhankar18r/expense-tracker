2ac8732f5fb2eda7cc26fe1ed420443c8be2b65f
01ricky18r <01ricky18r@gmail.com>
2022-08-29 12:58:21 +0530

    add expense button added

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,17 +1,43 @@
+import { useState } from "react";
+
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const ExpenseDataHandler = (data) => {
     const ExpenseData = {
       id: Math.random().toString(),
       ...data,
     };
     props.onAddExpense(ExpenseData);
+
+    setIsEditing(false);
+  };
+
+  const formChangeHandler = () => {
+    setIsEditing(true);
+  };
+  const formCloseHandler = () => {
+    setIsEditing(false);
   };
 
   return (
-    <div className="bg-purple-500  w-[60vw] m-auto mt-10">
-      <ExpenseForm onSaveExpenseData={ExpenseDataHandler} />
+    <div className="bg-purple-500  w-[60vw] m-auto mt-10 text-center">
+      {!isEditing ? (
+        <button
+          className="bg-purple-800 text-white p-3 m-4"
+          type="submit"
+          onClick={formChangeHandler}
+        >
+          Add expensess
+        </button>
+      ) : (
+        <ExpenseForm
+          onSaveExpenseData={ExpenseDataHandler}
+          onClose={formCloseHandler}
+        />
+      )}
     </div>
   );
 };
